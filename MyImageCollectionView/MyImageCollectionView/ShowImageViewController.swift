7fd8455bfed0a1a93984052cdd7cdd0f1fc3f458
TonyJem <a.jemanov@gmail.com>
2021-01-10 14:55:29 +0200

    Create "ShowImageViewController" class

--- MyImageCollectionView/MyImageCollectionView/ShowImageViewController.swift
+++ MyImageCollectionView/MyImageCollectionView/ShowImageViewController.swift
@@ -4,17 +4,15 @@ class ShowImageViewController: UIViewController {
     
     @IBOutlet weak var currentImage: UIImageView!
     
+    var imageName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentImage.image = UIImage(named: imageName)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setImageName(name: String) {
+        imageName = name
     }
-    */
 }
