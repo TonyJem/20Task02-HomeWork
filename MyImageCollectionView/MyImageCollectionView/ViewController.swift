40c32fef531047e4802a84c682aa4a02a2d33e66
TonyJem <a.jemanov@gmail.com>
2021-01-10 15:08:47 +0200

    Update "ViewController" with outlet and array

--- MyImageCollectionView/MyImageCollectionView/ViewController.swift
+++ MyImageCollectionView/MyImageCollectionView/ViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionViewOne: UICollectionView!
+    
+    let arrayTemperature = ["temp.green",
+                            "temp.blackGreen",
+                            "temp.lightYellow",
+                            "temp.darkYellow",
+                            "temp.orange",
+                            "temp.red"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
