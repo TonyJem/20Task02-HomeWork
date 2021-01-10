3ede38dde3ca3152ccc364ffd9638872f96124bf
TonyJem <a.jemanov@gmail.com>
2021-01-10 15:01:33 +0200

    Update "Cell" class

--- MyImageCollectionView/MyImageCollectionView/Cell.swift
+++ MyImageCollectionView/MyImageCollectionView/Cell.swift
@@ -4,7 +4,7 @@ class Cell: UICollectionViewCell {
     
     @IBOutlet weak var temperatureImage: UIImageView!
     
-    func setTemperatureImage() {
-        temperatureImage.image = UIImage(contentsOfFile: <#T##String#>)
+    func setTemperatureImage(tempName: String) {
+        temperatureImage.image = UIImage(named: tempName)
     }
 }
