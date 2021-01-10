6d3de966f3ffaea258122f163983887baeac7e96
TonyJem <a.jemanov@gmail.com>
2021-01-10 15:26:31 +0200

    Update ViewController.swift

--- MyImageCollectionView/MyImageCollectionView/ViewController.swift
+++ MyImageCollectionView/MyImageCollectionView/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-
+    
     @IBOutlet weak var collectionViewOne: UICollectionView!
     
     let arrayTemperature = ["temp.green",
@@ -22,7 +22,20 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = UICollectionViewCell()
-        return cell
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellOne", for: indexPath) as? Cell {
+            let imageName = arrayTemperature[indexPath.row]
+            cell.setTemperatureImage(tempName: imageName)
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "ShowImageViC") as? ShowImageViewController else { return }
+        var currentSelectedImage: String!
+        currentSelectedImage = arrayTemperature[indexPath.row]
+        vc.setImageName(name: currentSelectedImage)
+        present(vc, animated: true, completion: nil)
+        
     }
 }
